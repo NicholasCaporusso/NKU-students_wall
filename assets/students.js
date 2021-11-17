339fb7925bdcbf0f2c4db17455d280bfb0e38935
RegishWadum <mkschnappcs@gmail.com>
2021-11-17 16:07:37 -0500

    Matt Schnapp added his material

--- assets/students.js
+++ assets/students.js
@@ -19,5 +19,5 @@ var students=[
 '2021-spring-INF286-001_Justin_Walter',
 '2021-spring-INF286-002_Geo_Esquivel',
 '2021-Summer-INF286-001_LaShondra_Schwab',
-'2021-Fall-INF286-002_Matthew_Schnapp'
+'2021-Fall-INF286-002-Matthew_Schnapp'
 ];
